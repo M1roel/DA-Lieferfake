914119c17cfe467db6c26128e9af3e399559bd97
M1roel <157500795+M1roel@users.noreply.github.com>
2024-06-17 16:22:53 +0200

    Add data.js

---
+++ src/js/data.js
@@ -0,0 +1,12 @@
+let data = [
+    {
+        pizza: 'Pizza Margaritha',
+        price: 8.99,
+        amount: 1,
+        ingredients: [
+            {extra_1: 'Käse', cost: 2},
+            {extra_2: 'Knoblauch', cost: 0},
+            {extra_3: 'Chilli', cost: 0}
+        ]
+    }
+]
