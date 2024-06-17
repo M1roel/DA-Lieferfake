bc6d6671465fb20ff90285830dc6e26ad5fff738
M1roel <ppfautsch@yahoo.de>
2024-06-17 21:41:31 +0200

    function for adding menu works

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,21 @@
 let menues = data;
 
-show();
-
-
 function show() {
-    let orderElement = document.getElementById('orderElement');
-    orderElement.innerHTML = '';
+  let orderElement = document.getElementById("orderElement");
+  orderElement.innerHTML = "";
 
-    for (let i = 0; i < menues.length; i++) {
-        const menuType = menues.type[i];
-        const menuPrice = menues.price[i];
-        orderElement.innerHTML += generateMenu(menuType, menuPrice);        
-    }
+  for (let i = 0; i < menues.length; i++) {
+    const menuType = menues[i].type;
+    const menuPrice = menues[i].price;
+    orderElement.innerHTML += generateMenu(menuType, menuPrice);
+  }
 }
 
 function generateMenu(menuType, menuPrice) {
-    return `git
-    <div class='menu'>
-        <h3 class='menu-type'>${menuType}</h3>
-        <span class='menu-price'>${menuPrice}</span>
-    </div>
+  return `
+    <h3 class='menu-type'>${menuType}</h3>
+    <span class='menu-price'>${menuPrice}</span>
     `;
 }
+
+show();
