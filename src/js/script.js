cd7bd0f36cf3fb6e4111f2ee2fc28ebc01683d55
M1roel <ppfautsch@yahoo.de>
2024-06-17 21:10:46 +0200

    add functions to create the menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,24 @@
 let menues = data;
 
+show();
+
+
 function show() {
-    let orderElement = document.getElementById('order-element');
+    let orderElement = document.getElementById('orderElement');
     orderElement.innerHTML = '';
 
     for (let i = 0; i < menues.length; i++) {
         const menuType = menues.type[i];
         const menuPrice = menues.price[i];
-        orderElement.innerHTML += generateMenu(menuType, menuPrice, i);        
+        orderElement.innerHTML += generateMenu(menuType, menuPrice);        
     }
 }
 
 function generateMenu(menuType, menuPrice) {
-    return `
+    return `git
     <div class='menu'>
         <h3 class='menu-type'>${menuType}</h3>
         <span class='menu-price'>${menuPrice}</span>
     </div>
     `;
 }
-
-show();
