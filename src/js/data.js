3eb16200914fb7ca2a11858bfcb15319532f45a6
M1roel <ppfautsch@yahoo.de>
2024-06-18 17:06:30 +0200

    add ingredients to the order menu container with checkboxes

--- src/js/data.js
+++ src/js/data.js
@@ -6,9 +6,9 @@ let data = [
         discription: 'Knuspriger Pizzateig mit Tomatensauce, Käse und Oregano',
         img: '/public/img/margaritha.png',
         ingredients: [
-            {extra_1: 'Käse', cost: 2},
-            {extra_2: 'Knoblauch', cost: 0},
-            {extra_3: 'Chilli', cost: 0}
+            {name: 'Käse', cost: 2},
+            {name: 'Knoblauch', cost: 0.5},
+            {name: 'Chilli', cost: 0.5}
         ]
     },
     {
@@ -18,9 +18,9 @@ let data = [
         discription: 'mit frischen Champignons',
         img: '/public/img/funghi.png',
         ingredients: [
-            {extra_1: 'Käse', cost: 2},
-            {extra_2: 'Knoblauch', cost: 0},
-            {extra_3: 'Chilli', cost: 0}
+            {name: 'Käse', cost: 2},
+            {name: 'Knoblauch', cost: 0.5},
+            {name: 'Chili', cost: 0.5}
         ]
     }
 ]
--- src/js/script.js
+++ src/js/script.js
@@ -1,55 +1,62 @@
 let menues = data;
 
 function show() {
-  let orderElement = document.getElementById("orderElement");
-  orderElement.innerHTML = "";
+    let orderElement = document.getElementById("orderElement");
+    orderElement.innerHTML = "";
 
-  for (let i = 0; i < menues.length; i++) {
-    const menuType = menues[i].type;
-    const menuPrice = menues[i].price;
-    const menuDiscription = menues[i].discription;
-    const menuImg = menues[i].img;
-    orderElement.innerHTML += generateMenu(
-      menuType,
-      menuPrice,
-      menuDiscription,
-      menuImg,
-      i
-    );
-  }
+    for (let i = 0; i < menues.length; i++) {
+        const menuType = menues[i].type;
+        const menuPrice = menues[i].price;
+        const menuDiscription = menues[i].discription;
+        const menuImg = menues[i].img;
+        const ingredients = menues[i].ingredients;
+        orderElement.innerHTML += generateMenu(menuType, menuPrice, menuDiscription, menuImg, ingredients, i);
+    }
 }
 
-function generateMenu(menuType, menuPrice, menuDiscription, menuImg, i) {
-  return `
+function generateMenu(menuType, menuPrice, menuDiscription, menuImg, ingredients, i) {
+    return `
     <div class='menu'>
         <h3 class='menu-type'>${menuType}</h3>
         <span class='menu-price'>${menuPrice}€</span>
         <div class="add">
-            <button onclick='showOrder("${menuType}", ${menuPrice}, "${menuDiscription}", "${menuImg}")'>+</button>
+            <button onclick='showOrder("${menuType}", ${menuPrice}, "${menuDiscription}", "${menuImg}", ${JSON.stringify(ingredients)})'>+</button>
         </div>
     </div>
     `;
 }
 
-function showOrder(menuType, menuPrice, menuDiscription, menuImg) {
-  let showOrderElement = document.getElementById("orderHidden");
-  showOrderElement.classList.remove("d-none");
-  showOrderElement.innerHTML = `
+function showOrder(menuType, menuPrice, menuDiscription, menuImg, ingredients) {
+    let showOrderElement = document.getElementById('orderHidden');
+    showOrderElement.classList.remove('d-none');
+    let ingredientsHtml = '';
+
+    ingredients.forEach((ingredient, index) => {
+        ingredientsHtml += `
+            <div class="ingredient">
+                <input type="checkbox" id="extra_${index}" name="${ingredient.name}" value="${ingredient.name}">
+                <label for="extra_${index}">${ingredient.name} (+${ingredient.cost}€)</label>
+            </div>
+        `;
+    });
+
+    showOrderElement.innerHTML = `
     <div class="order">
-      <div class="order-head">
-        <button class="order-btn" onclick='closeOrder()'>X</button>
-        <h3 class='order-menu-type'>${menuType}</h3>
-        <img src="${menuImg}" alt="${menuType}>
-        <span class="order-discription">"${menuDiscription}"</span>
-        <span class='order-menu-price'>${menuPrice}€</span>
-      </div>
+        <div class="order-head">
+            <button class="order-btn" onclick='closeOrder()'>X</button>
+            <h3 class='order-menu-type'>${menuType}</h3>
+            <img src="${menuImg}" alt="${menuType}">
+            <span class="order-discription">${menuDiscription}</span>
+            <span class='order-menu-price'>${menuPrice}€</span>
+            ${ingredientsHtml}
+        </div>
     </div>
-  `;
+    `;
 }
 
 function closeOrder() {
-  let showOrderElement = document.getElementById("orderHidden");
-  showOrderElement.classList.add("d-none");
+    let showOrderElement = document.getElementById('orderHidden');
+    showOrderElement.classList.add('d-none');
 }
 
 show();
