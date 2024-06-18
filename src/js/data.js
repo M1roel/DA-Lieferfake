7d4632107004575a9e4d3fe7c01ad8f2348643e8
M1roel <ppfautsch@yahoo.de>
2024-06-18 16:29:04 +0200

    add images to data.js and order-menu

--- src/js/data.js
+++ src/js/data.js
@@ -4,6 +4,7 @@ let data = [
         price: 8.99,
         amount: 1,
         discription: 'Knuspriger Pizzateig mit Tomatensauce, Käse und Oregano',
+        img: '/public/img/margaritha.png',
         ingredients: [
             {extra_1: 'Käse', cost: 2},
             {extra_2: 'Knoblauch', cost: 0},
@@ -15,6 +16,7 @@ let data = [
         price: 11.99,
         amount: 1,
         discription: 'mit frischen Champignons',
+        img: '/public/img/funghi.png',
         ingredients: [
             {extra_1: 'Käse', cost: 2},
             {extra_2: 'Knoblauch', cost: 0},
--- src/js/script.js
+++ src/js/script.js
@@ -8,40 +8,48 @@ function show() {
     const menuType = menues[i].type;
     const menuPrice = menues[i].price;
     const menuDiscription = menues[i].discription;
-    orderElement.innerHTML += generateMenu(menuType, menuPrice, menuDiscription, i);
+    const menuImg = menues[i].img;
+    orderElement.innerHTML += generateMenu(
+      menuType,
+      menuPrice,
+      menuDiscription,
+      menuImg,
+      i
+    );
   }
 }
 
-function generateMenu(menuType, menuPrice, menuDiscription, i) {
+function generateMenu(menuType, menuPrice, menuDiscription, menuImg, i) {
   return `
     <div class='menu'>
         <h3 class='menu-type'>${menuType}</h3>
         <span class='menu-price'>${menuPrice}€</span>
         <div class="add">
-            <button onclick='showOrder("${menuType}", ${menuPrice}, "${menuDiscription}")'>+</button>
+            <button onclick='showOrder("${menuType}", ${menuPrice}, "${menuDiscription}", "${menuImg}")'>+</button>
         </div>
     </div>
     `;
 }
 
-function showOrder(menuType, menuPrice, menuDiscription) {
-  let showOrderElement = document.getElementById('orderHidden');
-  showOrderElement.classList.remove('d-none');
+function showOrder(menuType, menuPrice, menuDiscription, menuImg) {
+  let showOrderElement = document.getElementById("orderHidden");
+  showOrderElement.classList.remove("d-none");
   showOrderElement.innerHTML = `
     <div class="order">
       <div class="order-head">
-        <button onclick='closeOrder()'>X</button>
-        <h3 class='menu-type'>${menuType}</h3>
+        <button class="order-btn" onclick='closeOrder()'>X</button>
+        <h3 class='order-menu-type'>${menuType}</h3>
+        <img src="${menuImg}" alt="${menuType}>
         <span class="order-discription">"${menuDiscription}"</span>
-        <span class='menu-price'>${menuPrice}€</span>
+        <span class='order-menu-price'>${menuPrice}€</span>
       </div>
     </div>
   `;
 }
 
 function closeOrder() {
-  let showOrderElement = document.getElementById('orderHidden');
-  showOrderElement.classList.add('d-none');
+  let showOrderElement = document.getElementById("orderHidden");
+  showOrderElement.classList.add("d-none");
 }
 
 show();
